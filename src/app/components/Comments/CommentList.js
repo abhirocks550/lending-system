2c0c767c198ca1700fa1b06ed55666346ed9b60f
abhirocks550 <abhirocks550@gmail.com>
2018-04-12 15:15:07 +0530

    Add pagination for comments component

--- src/app/components/Comments/CommentList.js
+++ src/app/components/Comments/CommentList.js
@@ -7,7 +7,7 @@ const CommentList = (props) => {
             <h4>Posts List</h4>
             <div className="row" >
                 {
-                    props.CommentReducer.data.map((post, index) => {
+                    props.data.map((post, index) => {
                         return (
                             <div className="col-md-6 posts" key={index}>
                                 <div className="panel panel-primary">
--- src/app/components/Comments/Comments.js
+++ src/app/components/Comments/Comments.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import * as CommentsActions from '../../actions/commentsActions';
 import { connect } from 'react-redux';
 import CommentList from './CommentList';
+import ReactPaginate from 'react-paginate';
 
 class Comments extends Component {
+  constructor() {
+    super();
+    this.state = {
+        pageCount: 50,
+        offset: 0,
+        data: [],
+      };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.CommentReducer.data.length > 0 && (this.props.CommentReducer.data != nextProps.CommentReducer.data) && this.state.offset === 0) {
+      let firstData = nextProps.CommentReducer.data.slice(0, 10);
+      this.setState({
+          data: firstData,
+        });
+    }
+  }
+
+  handlePageClick = (data) => {
+        let selected = data.selected + 1;
+        let offset = Math.ceil(selected * 10);
+        let filteredPosts = this.props.CommentReducer.data.slice(((selected - 1) * 10),
+         selected * 10);
+        this.setState(
+            {
+                offset: offset,
+                data: filteredPosts,
+              });
+      };
+
   componentDidMount() {
     this.props.getComments();
   }
@@ -13,7 +44,18 @@ class Comments extends Component {
     return (
       <div>
         <h3>Comments</h3>
-        <CommentList {...this.props} />
+        <CommentList data={this.state.data} />
+        <ReactPaginate previousLabel={'previous'}
+                    nextLabel={'next'}
+                    breakLabel={<a href=''>...</a>}
+                    breakClassName={'break-me'}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={3}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={'pagination'}
+                    subContainerClassName={'pages pagination'}
+                    activeClassName={'active'} />
       </div>
     );
   }
